d9aabeee51a9891d81c6d28d4959227e1d04ccca
evan203 <evancreeper203@gmail.com>
2020-07-14 23:14:35 -0500

    Update start.sh
    clean up things

--- start.sh
+++ start.sh
@@ -85,10 +85,10 @@ else
 	mem=$mcram
 fi
 
-
-
 echo "Minecraft server is starting!"
 
 OPTS="-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true"
 
 java -Xms"$mem"G -Xmx"$mem"G $OPTS -jar $newFile
+
+# https://www.shellcheck.net/
