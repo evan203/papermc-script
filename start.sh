96c73323efbabc1da66531105a8dad9d3dafae58
evan203 <63885438+evan203@users.noreply.github.com>
2022-07-05 17:54:54 -0500

    Version 2.4
    fix flags for real this time, using flags from this source: https://www.reddit.com/r/admincraft/comments/npib9k/java_16_minecraft_server_117/

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@
 mcversion="1.19"
 project="paper"
 mem="1400M"
-JAVA="/usr/bin/java"
+JAVA="/usr/bin/java" # get latest java bins from https://adoptium.net/download and point this to the extracted binary
 
 pwdRoot="$(pwd)"
 
@@ -48,6 +48,6 @@ rm -rf $tmpDir
 
 echo "Minecraft server is starting!"
 
-FLAGS="-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Daikars.new.flags=true -Dusing.aikars.flags=https://mcflags.emc.gs"
+FLAGS="-XX:+UnlockExperimentalVMOptions -XX:+UseZGC -XX:+ZProactive -XX:ZCollectionInterval=600 -XX:+UseLargePages -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -XX:+PerfDisableSharedMem"
 
-${JAVA} -Xmx"$mem" -Xms"$mem" "${FLAGS}" -jar "$newFile" --nogui
+${JAVA} -server -Xmx"$mem" -Xms"$mem" ${FLAGS} -jar "$newFile" --nogui
